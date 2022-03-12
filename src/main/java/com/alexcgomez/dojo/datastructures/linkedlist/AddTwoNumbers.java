24b8d2675dd117f6ea3facaa325d0a37a09ed142
Alex Cáceres <alexcaceres93@gmail.com>
2022-03-12 15:38:43 +0100

    Created new problem

---
+++ src/main/java/com/alexcgomez/dojo/datastructures/linkedlist/AddTwoNumbers.java
@@ -0,0 +1,9 @@
+package com.alexcgomez.dojo.datastructures.linkedlist;
+
+public class AddTwoNumbers
+{
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return new ListNode(l1.val + l2.val);
+
+    }
+}
---
+++ src/test/java/com/alexcgomez/dojo/datastructures/linkedlist/AddTwoNumbersTest.java
@@ -0,0 +1,7 @@
+package com.alexcgomez.dojo.datastructures.linkedlist;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddTwoNumbersTest {
+
+}
