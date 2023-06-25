6d28acf2e2e6e1910040e6fe551e9a2ca532bf16
Alex Cáceres <alexcaceres93@gmail.com>
2023-06-25 21:09:06 +0200

    refactor laracast

---
+++ php/laracasts-php-path/src/Database.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace laracasts-php - path\src;
+
+class Database
+{
+    public $connection;
+
+    public function __construct(array $config)
+    {
+        $this->connection = new PDO(
+            "mysql:host={$config['host']};port={$config['port']};dbname={$config['name']};charset={$config['charset']}",
+            $config['user'],
+            $config['pass']
+        );
+    }
+
+    public function query($sql)
+    {
+        $statement = $this->connection->prepare($sql);
+        $statement->execute();
+
+        return $statement->fetchAll();
+    }
+}
